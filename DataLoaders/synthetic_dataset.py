e97ff6581cbc539688e7e67596a568aed787ce0d
bertinus <paul.f.bertin@gmail.com>
2020-01-21 10:18:29 -0500

    Created synthetic dataset file

---
+++ DataLoaders/synthetic_dataset.py
@@ -0,0 +1,18 @@
+from torch.utils.data import Dataset
+
+
+########################################################################################################################
+# Synthetic dataset
+########################################################################################################################
+
+
+class SyntheticDataset(Dataset):
+
+    def __init__(self):
+        return
+
+    def __len__(self):
+        return len(self.Data)
+
+    def __getitem__(self, idx):
+        return
