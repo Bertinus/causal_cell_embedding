37d954b643a8687629ab79e4062f56de67afd23b
bertinus <paul.f.bertin@gmail.com>
2020-07-03 11:30:56 -0400

    adapted code for GPU

--- ai/causalcell/training.py
+++ ai/causalcell/training.py
@@ -78,7 +78,7 @@ def train_epoch(model, device, train_loader, optimizer, epoch):
             loss.backward()
             optimizer.step()
 
-    all_loss = np.array(all_loss).mean()
+    all_loss = float(torch.mean(torch.tensor(all_loss)).detach().numpy())
     print('epoch {} Mean train loss: {:.4f}'.format(
         epoch, all_loss))
 
@@ -106,7 +106,7 @@ def evaluate_epoch(model, device, data_loader, epoch):
             loss = sum(losses.values())
             all_loss.append(loss)
 
-    all_loss = np.array(all_loss).mean()
+    all_loss = float(torch.mean(torch.tensor(all_loss)).detach().numpy())
     print('epoch {} Mean valid loss: {:.4f}'.format(
         epoch, all_loss))
 
@@ -185,75 +185,75 @@ def train(cfg):
 
     save_results(results, output_dir)
 
-
-def train_skopt(cfg, base_estimator, n_initial_points, random_state):
-    """
-    Do a Bayesian hyperparameter optimization
-    """
-
-    # Parse the parameters that we want to optimize, then flatten list.
-    hp_args = OrderedDict(configuration.parse_dict(cfg))
-    all_vals = []
-    for val in hp_args.values():
-        if isinstance(val, list):
-            all_vals.extend(val)
-        else:
-            all_vals.append(val)
-
-    hp_opt = skopt.Optimizer(dimensions=all_vals,
-                             base_estimator=base_estimator,
-                             n_initial_points=n_initial_points,
-                             random_state=random_state)
-
-    set_seed(cfg['seed'])
-
-    # best_valid and best_test score are used inside of train(), best_model
-    # score is only used in train_skopt() for final model selection.
-    state = {'base_iteration': 0,
-             'base_epoch': 0,
-             'best_valid_score': 0,
-             'best_test_score': 0,
-             'best_model_score': 0,
-             'best_epoch': 0,
-             'hp_opt': hp_opt,
-             'hp_args': hp_args,
-             'base_cfg': cfg,
-             'this_cfg': None,
-             'numpy_seed': None,
-             'optimizer': None,
-             'python_seed': None,
-             'scheduler': None,
-             'model': None,
-             'stats': None,
-             'metrics': [],
-             'torch_seed': None,
-             'suggestion': None,
-             'best_model': None}
-
-    # Do a bunch of loops.
-
-
-    for iteration in range(state['base_iteration'], n_iter + 1):
-
-        # Will not be true if training crashed for an iteration.
-        if isinstance(state['this_cfg'], type(None)):
-            suggestion = hp_opt.ask()
-            state['this_cfg'] = generate_config(cfg, hp_args, suggestion)
-            state['suggestion'] = suggestion
-        try:
-            this_valid_score, this_test_score, this_best_epoch, results, state = train(state['this_cfg'])
-
-            # Skopt tries to minimize the valid score, so it's inverted.
-            this_metric = this_valid_score * -1
-            hp_opt.tell(state['suggestion'], this_metric)
-        except RuntimeError as e:
-            # Something went wrong, (probably a CUDA error).
-            this_metric = 0.0
-            this_valid_score = 0.0
-            print("Experiment failed:\n{}\nAttempting next config.".format(e))
-            hp_opt.tell(suggestion, this_metric)
-
-        if this_valid_score > state['best_model_score']:
-            print("*** new best model found: score={}".format(this_valid_score))
-            state['best_model_score'] = this_valid_score
-            save_results(results, output_dir)
+#
+# def train_skopt(cfg, base_estimator, n_initial_points, random_state):
+#     """
+#     Do a Bayesian hyperparameter optimization
+#     """
+#
+#     # Parse the parameters that we want to optimize, then flatten list.
+#     hp_args = OrderedDict(configuration.parse_dict(cfg))
+#     all_vals = []
+#     for val in hp_args.values():
+#         if isinstance(val, list):
+#             all_vals.extend(val)
+#         else:
+#             all_vals.append(val)
+#
+#     hp_opt = skopt.Optimizer(dimensions=all_vals,
+#                              base_estimator=base_estimator,
+#                              n_initial_points=n_initial_points,
+#                              random_state=random_state)
+#
+#     set_seed(cfg['seed'])
+#
+#     # best_valid and best_test score are used inside of train(), best_model
+#     # score is only used in train_skopt() for final model selection.
+#     state = {'base_iteration': 0,
+#              'base_epoch': 0,
+#              'best_valid_score': 0,
+#              'best_test_score': 0,
+#              'best_model_score': 0,
+#              'best_epoch': 0,
+#              'hp_opt': hp_opt,
+#              'hp_args': hp_args,
+#              'base_cfg': cfg,
+#              'this_cfg': None,
+#              'numpy_seed': None,
+#              'optimizer': None,
+#              'python_seed': None,
+#              'scheduler': None,
+#              'model': None,
+#              'stats': None,
+#              'metrics': [],
+#              'torch_seed': None,
+#              'suggestion': None,
+#              'best_model': None}
+#
+#     # Do a bunch of loops.
+#
+#
+#     for iteration in range(state['base_iteration'], n_iter + 1):
+#
+#         # Will not be true if training crashed for an iteration.
+#         if isinstance(state['this_cfg'], type(None)):
+#             suggestion = hp_opt.ask()
+#             state['this_cfg'] = generate_config(cfg, hp_args, suggestion)
+#             state['suggestion'] = suggestion
+#         try:
+#             this_valid_score, this_test_score, this_best_epoch, results, state = train(state['this_cfg'])
+#
+#             # Skopt tries to minimize the valid score, so it's inverted.
+#             this_metric = this_valid_score * -1
+#             hp_opt.tell(state['suggestion'], this_metric)
+#         except RuntimeError as e:
+#             # Something went wrong, (probably a CUDA error).
+#             this_metric = 0.0
+#             this_valid_score = 0.0
+#             print("Experiment failed:\n{}\nAttempting next config.".format(e))
+#             hp_opt.tell(suggestion, this_metric)
+#
+#         if this_valid_score > state['best_model_score']:
+#             print("*** new best model found: score={}".format(this_valid_score))
+#             state['best_model_score'] = this_valid_score
+#             save_results(results, output_dir)
